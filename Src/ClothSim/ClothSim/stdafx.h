6edc76d07993085d04bfdc0cdae55552dc94754f
jjuiddong <jjuiddong@hanmail.net>
2019-12-12 18:12:42 +0900

    update base project

---
+++ Src/ClothSim/ClothSim/stdafx.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../../../../Common/Common/common.h"
+#include "../../../../Common/Graphic11/graphic11.h"
+#include "../../../../Common/Framework11/framework11.h"
+
+
+using common::StrId;
+using common::StrPath;
+using common::Vector3;
+using common::Vector2;
+using common::sRectf;
+
+//#include "global.h"
+//extern cGlobal *g_global;
